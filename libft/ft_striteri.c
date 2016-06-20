6e41ed1504401c727d16454a7eee08936ec2abc1
Matthew <myshorts.mail@gmail.com>
2016-06-20 20:24:16 +0200

    began t_list functions, fixed ft_memcpy, updated fixing

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -10,12 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	int		cnt;
 
 	if (s == NULL || f == NULL)
-		return (NULL);
+		return;
 	cnt = 0;
 	while (s[cnt] != '\0')
 	{
--- libft/libft.h
+++ libft/libft.h
@@ -20,6 +20,13 @@
 
 # define BUFF_SIZE 32
 
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+
 char	*ft_appendc(char *str, int c);
 char	*ft_itoa(int n);
 char	*ft_itobase(int n, int base);
@@ -72,7 +79,7 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memalloc(size_t size);
 void	*ft_memccpy(void *dst, void *src, int c, size_t n);
 void	*ft_memchr(void *s, int c, size_t n);
-void	*ft_memcpy(void *dest, void *src, size_t n);
+void	*ft_memcpy(void *dest, void const *src, size_t n);
 void	ft_memdel(void **ap);
 void	*ft_memmove(void *dst, void *src, size_t len);
 void	*ft_memset(void *s, int c, size_t n);
@@ -89,5 +96,12 @@ void	ft_strclr(char *s);
 void	ft_strdel(char **as);
 void	ft_striter(char *s, void (*f)(char *));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+
+t_list	*ft_lstnew(void const *content, size_t content_size);
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void	ft_lstadd(t_list **alst, t_list *new);
+void	ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 
 #endif
