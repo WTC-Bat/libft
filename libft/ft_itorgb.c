80e9b42ab641e84cfbf5b03d50f80aaa9ad87e8c
Matthew Williams <FatBat@Matthews-iMac.local>
2016-09-29 21:12:49 +0200

    added rgbtoi and itorgb

---
+++ libft/ft_itorgb.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+int		*itorgb(int rgbint)
+{
+	//	if rgbint < 0?
+	//	if rgbint > x?
+	int		r;
+	int		g;
+	int		b;
+	int		*rgb;
+
+	r = 0;
+	g = 0;
+	b = 0;
+	rgb = (int *)malloc(sizeof(int) * 3);
+	// r = (rgbint >> 16) & 255;
+	// g = (rgbint >> 8) & 255;
+	// b = rgb & 255;
+	rgb[0] = (rgbint >> 16) & 255;
+	rgb[1] = (rgbint >> 8) & 255;
+	rgb[2] = rgbint & 255;
+	return (rgb);
+}
---
+++ libft/ft_rgbtoi.c
@@ -0,0 +1,21 @@
+int		rgbtoi(int red, int green, int blue)
+{
+	int		r;
+	int		g;
+	int		b;
+	int		rgbint;
+
+	r = 0;
+	g = 0;
+	b = 0;
+	if (red <= 255 && red >= 0)
+		r = red;
+	if (green <= 255 && green >= 0)
+		g = green;
+	if (blue <= 255 && blue >= 0)
+		b = blue;
+	rgbint = r;
+	rgbint = (rgbint << 8) + g;
+	rgbint = (rgbint << 8) + b;
+	return (rgbint);
+}
